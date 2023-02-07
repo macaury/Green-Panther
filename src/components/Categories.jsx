dacea81d7f27f2787741f4ae51b67d19d76bd0c0
macaury carvalho <carvalhomacaury@gmail.com>
2023-02-07 18:48:22 -0300

    update

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,10 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import Source from "../../dist/assets/image.png";
-import Source2 from "../../dist/assets/image2.png";
-import Source3 from "../../dist/assets/image3.png";
-import Source4 from "../../dist/assets/image4.png";
+
 
 function Catergories() {
 
@@ -22,23 +19,23 @@ function Catergories() {
         <Row className="categories_container_card ">
           <Col  lg="6" md="2" xs="5" >
             <div class="card_categories">
-              <img src={Source} alt=""   className="img_cate"/>
+              <img src="https://github.com/macaury/Green-Panther/blob/main/dist/assets/image2.png?raw=true" alt=""   className="img_cate"/>
             </div>
           </Col>
           <Col  lg="6" md="2" xs="5">
             <div class="card_categories ">
-              <img src={Source2} alt="" className="img_cate" />
+              <img src="https://github.com/macaury/Green-Panther/blob/main/dist/assets/image.png?raw=true"alt="" className="img_cate" />
             </div>
           </Col>
 
           <Col col="3" lg="6" md="2"  xs="5">
             <div class="card_categories ">
-              <img src={Source3} alt=""  className="img_cate"/>
+              <img src="https://github.com/macaury/Green-Panther/blob/main/dist/assets/image3.png?raw=true" alt=""  className="img_cate"/>
             </div>
           </Col>
           <Col col="3" lg="6" md="2" xs="5">
             <div class="card_categories">
-              <img src={Source4} alt=""  className="img_cate" />
+              <img src="https://github.com/macaury/Green-Panther/blob/main/dist/assets/image4.png?raw=true" alt=""  className="img_cate" />
             </div>
           </Col>
         </Row>
